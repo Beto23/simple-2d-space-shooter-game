7a3edf20f8b4ede426ce01c97c1afe9949836d5e
Jose Alberto <josee-45@hotmail.com>
2017-11-04 00:16:00 -0600

    iniciar canvas (getcontext2d)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-function greeter(person:string) {
-    console.log(`Hello ${person}`);
+window.addEventListener('load', init);
+
+function init () : void {
+    console.log('cargado');
+    const canvas : any = document.getElementById('micanvas');
+    const ctx = canvas.getContext('2d'); //context canvas
+    ctx.fillRect(20,20,50,50); // Dibujando cuadrado
 }
-
-let user = "Beto user";
-
-greeter(user);
