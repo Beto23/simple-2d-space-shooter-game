deb22591ba7a37dd717645d0aeab19e67c26da60
Jose Alberto <josee-45@hotmail.com>
2017-11-04 10:09:10 -0600

    Dibujar nave

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 // Objetos importantes de canvas
 const canvas : any = document.getElementById('main');
 const ctx = canvas.getContext('2d');
+console.log(ctx);
+
+// creando objeto de la nave (class)
+const nave = {
+    x: 100,
+    y: canvas.height -70,
+    width: 50,
+    height: 50
+}
 
 //Definicion de variables para imagenes
 let fondo : any;
@@ -14,12 +23,20 @@ function loadMedia() : void {
     }
 }
 
-function drawBackgraoud() : void {
+function dibujarFondo() : void {
     ctx.drawImage(fondo,0,0)
 }
 
+function dibujarNave() : void {
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(nave.x, nave.y, nave.width, nave.height);
+    ctx.restore();
+}
+
 function frameLoop() : void {
-    drawBackgraoud();
+    dibujarFondo();
+    dibujarNave();
 }
 
 //Ejecucion de funciones
